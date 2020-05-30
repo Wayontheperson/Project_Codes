7654be47e46d5a5c88e304c3d9fcfe3f39d026ee
Park.JH <ntcf652@gmail.com>
2020-05-30 20:41:48 +0900

    Initial commit

---
+++ StarCraft_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarCraft_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
